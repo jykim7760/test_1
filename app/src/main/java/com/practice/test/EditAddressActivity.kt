ed37546f368e73c9e2a4c15365aba3b506715446
jykim7760 <jykim7760@naver.com>
2020-04-08 22:21:07 +0900

    데이터입력화면에서 가지고 돌아오기

--- app/src/main/java/com/practice/test/EditAddressActivity.kt
+++ app/src/main/java/com/practice/test/EditAddressActivity.kt
@@ -1,12 +1,24 @@
 package com.practice.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_address.*
 
 class EditAddressActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_address)
+
+        okBtn.setOnClickListener {
+
+            val resultsIntent = Intent()
+            resultsIntent.putExtra("mainAddress", )
+
+
+
+
+        }
     }
 }
--- app/src/main/java/com/practice/test/MainActivity.kt
+++ app/src/main/java/com/practice/test/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.practice.test
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//1000이라는 숫자가, 주소를 얻기위한 요청이라고 메모
     val REQ_FOR_ADDRESS = 1000
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,4 +32,21 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        요청종류확인
+        if (requestCode == REQ_FOR_ADDRESS)
+//    확인을 눌러서 종료된게 맞는지
+            if (resultCode == Activity.RESULT_OK)
+//                맞으면 첨부된 데이터를 받자
+
+                val main = data?.getStringExtra("mainAddress")
+        val detail = data?.getStringExtra("detailAddress")
+
+        addressTxt.text = "${main} / ${detail}"
+
+    }
+
 }
